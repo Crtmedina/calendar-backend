1de9e698e5db777bd8771ed8715a6a8ceda88f97
Cristobal Medina <cristobal.medina.1997@gmail.com>
2023-10-26 00:05:02 -0300

    first commit

---
+++ routes/events.js
@@ -0,0 +1,40 @@
+/*
+    Event Routes
+    /api/events
+*/
+
+const { Router } = require('express');
+const { check  } = require('express-validator');
+
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { crearEvento, actualizarEvento, eliminarEvento, getEvento } = require('../controllers/events');
+
+const  router  = Router();
+
+
+// Todas tienen que pasar por la validacion del JWT
+router.use( validarJWT );
+
+// Obtener eventos
+router.get('/', getEvento );
+
+// Crear nuevo eventos
+router.post(
+    '/',
+    [
+        check('title','El titulo es obligatorio').not().isEmpty(),
+        check('start','Fecha de inicio es obligatoria').custom( isDate ),
+        check('end','Fecha de finalizacion es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento );
+
+// Crear nuevo eventos
+router.put('/:id', validarJWT, actualizarEvento );
+
+// Crear nuevo eventos
+router.delete('/:id',  eliminarEvento );
+
+module.exports = router;
